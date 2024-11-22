8d7b3e3ffaac143afa7a9765645a0fcff4ce3a2e
User <User@DESKTOP-B739F08>
2024-11-22 15:40:20 +0530

    Add project files.

---
+++ AntiForgeryToken/Models/BankAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace AntiForgeryToken.Models
+{
+    public class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public int PIN { get; set; }
+    }
+}
---
+++ AntiForgeryToken/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AntiForgeryToken.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
